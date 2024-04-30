bd50ef98653abda07a292661209b102a4680f65e
tvictor <victor.thiebaut@ynov.com>
2024-04-30 12:37:54 +0200

    météo

--- src/main/java/org/capitalism/Arenas/Arena.java
+++ src/main/java/org/capitalism/Arenas/Arena.java
@@ -2,6 +2,8 @@ package org.capitalism.Arenas;
 
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
+
+import org.bukkit.Bukkit;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.capitalism.Capitalism;
 import org.capitalism.Prospectors.Prospector;
@@ -50,6 +52,15 @@ public class Arena {
         new BukkitRunnable(){
             @Override
             public void run() {
+                 if (gameDuration == 360) {
+                    Bukkit.getWorld("world").setStorm(true);
+                } else if (gameDuration == 240) {
+                    Bukkit.getworld("world").setThunder(true);
+                } else if (gameduration == 201) {
+                    for (Player p : Bukkit.getOnlinePlayers()) {
+                        p.playSound(p.getlocation, "cptlm:endgame",1,1);
+                    } 
+                }
                 if(gameDuration > 0){
                     gameDuration--;
                     for(Prospector prospector : prospectorsList){
